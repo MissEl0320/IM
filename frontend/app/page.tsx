b0777ab6b09a95fb4c216502f4c6cf1982608779
Jan-creatorr <jannerudabenolirao@gmail.com>
2026-05-30 04:33:07 +0800

    safely wrap serverless initialization queries

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -107,7 +107,7 @@ export default function Home() {
 
     if (view === "forgot") {
       try {
-        const response = await fetch("https://im-1-o5b7.onrender.com/api/forgot-password", {
+        const response = await fetch("http://localhost:5000/api/forgot-password", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email: formData.email }),
@@ -125,7 +125,7 @@ export default function Home() {
     }
     else if (view === "verify_code") {
       try {
-        const response = await fetch("https://im-1-o5b7.onrender.com/api/reset-password", {
+        const response = await fetch("http://localhost:5000/api/reset-password", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -151,7 +151,7 @@ export default function Home() {
         return;
       }
       try {
-        const response = await fetch("https://im-1-o5b7.onrender.com/api/signup", {
+        const response = await fetch("http://localhost:5000/api/signup", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -174,7 +174,7 @@ export default function Home() {
     } 
     else if (view === "login") {
       try {
-        const response = await fetch("https://im-1-o5b7.onrender.com/api/login", {
+        const response = await fetch("http://localhost:5000/api/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -526,7 +526,7 @@ export default function Home() {
                   </ul>
                 </div>
 
-
+                
               </div>
             </div>
         )}
